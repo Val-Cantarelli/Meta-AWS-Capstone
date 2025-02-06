3b77972581b56ad435561148e572907705094f68
Valdielen Casarin <valcantarellic@gmail.com>
2025-02-06 14:54:41 -0500

    Deploy Lambda and Test API Responses

--- backend/LittleLemon/LittleLemonAPI/models.py
+++ backend/LittleLemon/LittleLemonAPI/models.py
@@ -11,7 +11,9 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255,db_index=True)
     price = models.DecimalField(max_digits=6,decimal_places=2,db_index=True)
     featured = models.BooleanField(db_index=True)
+    avalability = models.BooleanField(db_index=True,default=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
     
     def __str__(self):
         return self.title
--- backend/LittleLemon/LittleLemonAPI/urls.py
+++ backend/LittleLemon/LittleLemonAPI/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 
 
-router = DefaultRouter(trailing_slash=False)
+router = DefaultRouter(trailing_slash=True)
 router.register(r'menu-items', MenuItemsViewSet)
 router.register(r'categories', CategoryViewSet)
 router.register(r'groups/(?P<group_name>manager|delivery-crew)/users', ManagerViewSet, basename='group-users')
@@ -12,9 +12,9 @@ router.register(r'orders', OrdersViewSet, basename='orders')
 
 urlpatterns = [
     # This route allows the 'deletion' of a user from a specific group by passing the group name and user ID in the URL.DefaultRouter() doesnt 
-    path('groups/<str:group_name>/users/<int:user_id>', ManagerViewSet.as_view({'delete': 'destroy'}), name='group-user-detail'),
-    path('cart/menu-items', CartViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'}), name='cart-menu-items'),
-    path('cart/orders', OrdersViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'}), name='cart-orders'),
+    path('groups/<str:group_name>/users/<int:user_id>/', ManagerViewSet.as_view({'delete': 'destroy'}), name='group-user-detail'),
+    path('cart/menu-items/', CartViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'}), name='cart-menu-items'),
+    path('cart/orders/', OrdersViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'}), name='cart-orders'),
 ]
 
 urlpatterns += router.urls
