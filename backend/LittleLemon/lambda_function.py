11ac225a7455b54665b0dd575896384e1ec5c650
Valdielen Casarin <valcantarellic@gmail.com>
2025-02-07 11:39:15 -0500

    lamda_function updated

--- backend/LittleLemon/lambda_function.py
+++ backend/LittleLemon/lambda_function.py
@@ -5,5 +5,4 @@ from aws_lambda_wsgi import make_lambda_handler
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LittleLemon.settings')
 django.setup()
 
-# Cria o manipulador Lambda com o WSGI
 handler = make_lambda_handler()
