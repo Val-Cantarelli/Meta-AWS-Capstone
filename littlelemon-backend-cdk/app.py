e88fdb71bd2978f9cf2d0699397fc960b65d0d27
Valdielen Casarin <valcantarellic@gmail.com>
2025-05-15 19:20:46 -0400

    feat: associate Lambda to VPC private subnets and add SG

--- littlelemon-backend-cdk/app.py
+++ littlelemon-backend-cdk/app.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 import os
+from aws_cdk import App, Environment
+from lib.compute_stack import ComputeStack
+from lib.network_stack import NetworkStack
+from lib.database_stack import DatabaseStack
 
-from aws_cdk import App  
-from lib.compute_stack import ComputeStack 
+
+PROFILE_NAME = "mfa-profile"
+
+account = os.popen(f'aws sts get-caller-identity --query "Account" --output text --profile {PROFILE_NAME}').read().strip()
+region = os.popen(f'aws configure get region --profile {PROFILE_NAME}').read().strip()
+
+if not account or not region:
+    raise RuntimeError("Fail: verify AWS credentials")
+
+env = Environment(account=account, region=region)
 
 app = App()
 
-# Instanciates the ComputeStack
-ComputeStack(app, "ComputeStack")
-
-# Generates the CloudFormation's template
+ComputeStack(app, "ComputeStack", env=env)
+NetworkStack(app, "NetworkStack", env=env)
+DatabaseStack(app, "DatabaseStack", env=env)
 app.synth()
