2b40b0e2302a291306775df67c35de321683cbf6
Valdielen Casarin <valcantarellic@gmail.com>
2025-06-02 11:33:37 -0400

    fix: Update lambda_function.py and ALLOWED_HOSTS to enable health endpoint via API Gateway

--- backend/LittleLemon/LittleLemon/urls.py
+++ backend/LittleLemon/LittleLemon/urls.py
@@ -4,21 +4,10 @@ from django.urls import path,include
 from LittleLemon.LittleLemonAPI.views import health_check
 
 urlpatterns = [
-    path('', health_check),
+    path('health', health_check),
     path('admin/', admin.site.urls),
     path('api/', include('LittleLemon.LittleLemonAPI.urls')),
     path('auth/', include('djoser.urls')), 
     path('auth/', include('djoser.urls.jwt')),
     
 ]
-'''
-
-Registro de usuário: /auth/users/
-Login (JWT): /auth/jwt/create/
-Refresh de token JWT: /auth/jwt/refresh/
-Reset de senha: /auth/password/reset/
-
-Criação de Usuário POST /auth/users/
-
-
-'''
