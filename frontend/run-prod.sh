f7e62d91a99fcaac61f18aa4ee6b926476c3ccf3
Valdielen Casarin <valcantarellic@gmail.com>
2025-06-03 11:06:31 -0400

    Update API_BASE_URL to new API Gateway (Lambda) endpoint

--- frontend/run-prod.sh
+++ frontend/run-prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-DJANGO_ENV=production
-API_BASE_URL=https://xy3r212g98.execute-api.us-east-1.amazonaws.com/dev
+unset DJANGO_ENV
+export DJANGO_SETTINGS_MODULE=core.settings.production
+export API_BASE_URL=https://6qpkzrhv4c.execute-api.us-east-1.amazonaws.com/v1
 
 python manage.py collectstatic --noinput
 python manage.py runserver
