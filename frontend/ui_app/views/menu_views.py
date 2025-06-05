15592292847c99f9a247b77c570a8edbc2dd896c
Valdielen Casarin <valcantarellic@gmail.com>
2025-06-05 12:30:07 -0400

    refactor(frontend): remove authentication requirement from menu-items route

--- frontend/ui_app/views/menu_views.py
+++ frontend/ui_app/views/menu_views.py
@@ -1,11 +1,9 @@
 from django.contrib import messages
 
 import requests
-import logging
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from urllib.parse import urlparse, parse_qs
 from django.conf import settings
-from ui_app.api.auth_utils import refresh_access_token
 
 def menu(request):
     page = request.GET.get('page')
@@ -14,27 +12,11 @@ def menu(request):
     else:
         api_url = f"{settings.API_BASE_URL}/api/menu-items"
 
-    token = request.session.get('access')
-    if not token:
-        messages.warning(request, 'You need to log in first.')
-        logging.warning("Access token not found in session.")
-        return redirect('login')
-
-    headers = {'Authorization': f'Bearer {token}'}
-
-    response = requests.get(api_url, headers=headers)
+    # Não precisa mais de token nem headers para endpoint público
+    response = requests.get(api_url)
     print("Status:", response.status_code)
     print("Content:", response.text)
-    
-    if response.status_code in [401,403]:
-        new_access = refresh_access_token(request)
-        if new_access:
-            headers['Authorization'] = f'Bearer {new_access}'
-            response = requests.get(api_url, headers=headers)
-        if response.status_code in [401, 403]:
-            request.session.flush()
-            messages.warning(request, 'Session expired. Please log in again.')
-            return redirect('login')
+
     try:
         data = response.json()
     except Exception as e:
@@ -47,28 +29,21 @@ def menu(request):
 
     next_page = None
     previous_page = None
-    
+
     if data.get('next'):
         parsed_next = urlparse(data.get('next'))
         query_next = parse_qs(parsed_next.query)
         next_page = query_next.get('page', [None])[0]
-        
+
     if data.get('previous'):
         parsed_prev = urlparse(data.get('previous'))
         query_prev = parse_qs(parsed_prev.query)
         previous_page = query_prev.get('page', [None])[0]
-    
+
     context = {
         'items': items,
         'next_page': next_page,
         'previous_page': previous_page,
     }
-    
-    print("Headers:", headers)
-    print("Token:", token)
-    print("URL:", api_url)
-    print("Status:", response.status_code)
-    print("JSON:", data)
-    print("API URL:", api_url)
 
     return render(request, 'menu.html', context)
