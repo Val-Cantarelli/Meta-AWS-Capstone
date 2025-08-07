6f46d04fbb41616bb830c141ecc21077cafa2301
Val-Cantarelli <valcantarellic@gmail.com>
2025-08-06 21:39:00 -0400

    Add ebignore and fix the health check path on ebdeploy

--- frontend/ui_app/urls.py
+++ frontend/ui_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from ui_app.views.base_views import home, about, book
+from ui_app.views.base_views import  home, about, book, health
 from ui_app.views.auth_views import login_view,signup_view
 from ui_app.views.menu_views import menu
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('book/', book, name='book'),
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
+    path('health/', health, name='health'),
 ]
