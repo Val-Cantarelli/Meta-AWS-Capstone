b14f08fb41abae131ef0bb5cccf287726a0d49f9
Val-Cantarelli <valcanterellic@gmail.com>
2025-08-08 20:16:39 -0400

    switch to ASGI and serve /health at Nginx to keep targets healthy

--- frontend/core/settings/production.py
+++ frontend/core/settings/production.py
@@ -1,7 +1,6 @@
 from .base import *
 import os
-from dotenv import load_dotenv
-load_dotenv()
+
 
 DEBUG = False
 
@@ -13,6 +12,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 #(optional)SESSION_EXPIRE_AT_BROWSER_CLOSE = True 
+CSRF_TRUSTED_ORIGINS = ['https://www.vahltech.com','https://vahltech.com','https://*.elasticbeanstalk.com']
+SECURE_REDIRECT_EXEMPT = [r'^health/?$']
+
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-east-1")
@@ -46,3 +48,6 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/'
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+SECURE_SSL_REDIRECT = True
