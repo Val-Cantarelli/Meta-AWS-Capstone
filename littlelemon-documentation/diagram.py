213da0c23e59abcf4002474ca3bb1e49b5f17ea1
Val Cantarelli <valcantarellic@gmail.com>
2025-09-19 10:18:20 -0400

    docs: Diagram AWS architeture modified

--- littlelemon-documentation/diagram.py
+++ littlelemon-documentation/diagram.py
@@ -3,25 +3,24 @@ from diagrams.aws.compute import LambdaFunction, EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import APIGateway
 from diagrams.aws.storage import S3
-from diagrams.aws.devtools import CloudDevelopmentKit
 
 with Diagram("AWS Architecture - Little Lemon Project", show=True, direction="TB"):
 
-    with Cluster("VPC"):
-        with Cluster("Private Subnets"):
-            lambda_fn = LambdaFunction("Django API Lambda")
-            rds_proxy = RDS("RDS Proxy")
-            rds = RDS("MySQL RDS")
+    with Cluster("AWS Cloud"):
+        api_gateway = APIGateway("HTTP API Gateway")
+        s3 = S3("S3 Static Files")
+        beanstalk = EC2("Elastic Beanstalk Frontend")
+
+        with Cluster("VPC"):
+            with Cluster("Private Subnets"):
+                lambda_fn = LambdaFunction("Django API Lambda")
+                rds_proxy = RDS("RDS Proxy")
+                rds = RDS("MySQL RDS")
+                lambda_fn >> rds_proxy >> rds
+
             openvpn = EC2("OpenVPN Server")
 
-            lambda_fn >> rds_proxy >> rds
-
-        api_gateway = APIGateway("HTTP API Gateway")
-        lambda_fn >> api_gateway
-
-    s3 = S3("S3 Static Files")
-    beanstalk = EC2("Elastic Beanstalk Frontend")
-
-    beanstalk >> api_gateway
-    beanstalk >> s3
-    openvpn >> lambda_fn
+        api_gateway >> lambda_fn
+        beanstalk >> api_gateway
+        beanstalk >> s3
+        openvpn >> lambda_fn
